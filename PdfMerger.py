8785121601a84b4508b98412d9627da8401d8264
Kashaf-Ansari-3108 <kashafansari3108@gmail.com>
2025-08-04 17:05:19 +0500

    PdfMerger Added

---
+++ PdfMerger.py
@@ -0,0 +1,10 @@
+import PyPDF2
+
+pdFiles = ["1.pdf", "2.pdf"]
+merger = PyPDF2.PdfMerger()
+for filename in pdFiles:
+    pdFile = open(filename, 'rb')
+    pdfReader = PyPDF2.PdfReader(pdFile)
+    merger.append(pdfReader)
+pdFile.close()
+merger.write("merged.pdf")
